f23a6ed399c0935eb6d018d45e93c318052b6727
Camila Parise <camilinhaparis@hotmail.com>
2022-09-15 22:12:14 -0300

    CadastroAtualizado

--- cadastre.php
+++ cadastre.php
@@ -1,73 +1,94 @@
 <!doctype html>
+
 <html lang="pt-br">
+
   <head>
     <meta charset="utf-8">
     <title>Ajudai</title>
-    <link rel="stylesheet" href="css/estilo.css" >
-    
-</head>   
+    <link rel="stylesheet" href="css/form-validation.css" >  
+    <link href="css/bootstrap.min.css" rel="stylesheet">    
+  </head>   
    
-<body>
-  <!-----------seção principal do site------>
-<section id="form"> 
-<div class="img">
-  <a href="index.php" class="classe2"><img src="img/Ajudai.png" /></a>
-  </div> 
-  <br> 
-  <p class="cliente">Cadastro do Cliente </p>
-    <form action="cadastrar.php" method="post" enctype="multipart/form-data">
-      <label>
-      Nome:* 
-      </label>
-      <input type="text" name="nome" placeholder="Digite o seu nome completo" class="entradas" required><br>
+  <body class="bg-light">
 
-      <label>
-      Atividade:*
-      </label>
-      <input type="text" name="atividade" placeholder="Digite sua atividade" class="entradas" required><br>
+    <div class="container">
 
-      <label>
-      E-mail:*
-      </label>
-      <input type="text" name="email" placeholder="Digite o e-mail" class="entradas" required><br>
+        <!-----------seção principal do site------>
+      <section id="form"> 
+              <div class="py-5 text-center">
+                <img class="d-block mx-auto mb-4" src="img/Ajudai.png" >
+                <h2>Cadastro do Cliente</h2>      
+              </div>
 
-      <label>
-      Senha:*
-      </label>
-      <input type="password" name="senha" placeholder="Digite uma senha" class="entradas" required><br>
+          <div class="row g-5 ">  
+            
+            
+                  <form action="cadastrar.php" method="post" enctype="multipart/form-data">
+                    <div >
+                      <label>Nome:* </label>
+                      <input type="text" name="nome" placeholder="Digite o seu nome completo" class="form-control" required><br>
+                    </div>
 
-      <label>
-      Dica:*
-      </label>
-      <input type="text" name="dica" placeholder="Digite uma dica" class="entradas" required><br>
+                    <div class="col-12">
+                      <label class="form-label">Atividade:*</label>
+                      <input type="text" name="atividade" placeholder="Digite sua atividade" class="form-control" required><br>
+                    </div>
+                  
+                    <div class="col-12">
+                        <label class="form-label">E-mail:*</label>
+                        <input type="text" name="email" placeholder="Digite o e-mail" class="form-control" required><br>
+                        <div class="invalid-feedback">
+                              Please enter a valid email address for shipping updates.
+                        </div>
+                    </div>
 
-      <label>
-      Telefone:*
-      </label>
-      <input type="text" name="telefone" placeholder="Digite o seu telefone" class="entradas" required><br>
+                    <label class="form-label">Senha:*</label>
+                    <input type="password" name="senha" placeholder="Digite uma senha" class="form-control" required><br>
 
-      <label>
-      Endereco:*
-      </label>
-      <input type="text" name="endereco" placeholder="Digite o seu endereco" class="entradas" required><br>      
+                    <label class="form-label">Dica:*</label>
+                    <input type="text" name="dica" placeholder="Digite uma dica" class="form-control" required><br>
 
-      <label>
-      Imagem da capa:*
-      </label>
-      <input type="file" name="capa"  class="entradas_img" required><br>
+                    <label class="form-label">Telefone:*</label>
+                    <input type="text" name="telefone" placeholder="Digite o seu telefone" class="form-control" required><br>
 
-      <label>
-      Imagem de perfil:*
-      </label>
-      <input type="file" name="perfil"  class="entradas_img" required><br>
+                    <label class="form-label">Endereco:*</label>
+                    <input type="text" name="endereco" placeholder="Digite o seu endereco" class="form-control" required><br>      
 
-      <label>
-      <input type="submit" value="CADASTRAR" class="bt_form_cad">        
-      </label>
-    </form>  
-    <a href="perfil.php" >LOGAR</a><br>
-    
-</section>      
+                    <label class="form-label">Imagem da capa:*</label>
+                    <input type="file" name="capa"  class="entradas_img" required><br>
+                    <br>
 
-</body>
+                    <label class="form-label">Imagem de perfil:*</label>
+                    <input type="file" name="perfil"  class="entradas_img" required><br>
+                    
+                    <hr class="my-4">
+
+                    <div class="form-check">
+                      <input type="checkbox" class="form-check-input" id="same-address">
+                      <label class="form-check-label" for="same-address">Quero receber recomendações e dicas de aprendizado.</label>
+                    </div>
+
+                    <div class="form-check">
+                      <input type="checkbox" class="form-check-input" id="save-info">
+                      <label class="form-check-label" for="save-info">Ao se inscrever, você concorda com nossos Termos de Uso e com a Política de Privacidade.</label>
+                    </div>
+                    <br>                    
+
+                    <label><input type="submit" value="CADASTRAR" class="w-100 btn btn-primary btn-lg"></label>
+
+                    <hr class="my-4">
+                    <div class="text-center">
+                      <label >Já tem uma conta?<a href="perfil.php"><b> Faça Login</b></a><br></lael>
+                    </div>
+                </form>  
+                
+              
+          </div>  
+      </section>      
+      <footer class="my-5  text-muted text-center text-small">
+        <p class="mb-1">&copy; 2021–2022 Ajudaí</p>    
+      </footer>
+
+    </div>
+  </body>
 </html>
